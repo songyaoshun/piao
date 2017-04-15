3e784aee2349e9b1aaf809cc9f34b6009a2d6fc1
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 13:46:19 +0800

    update

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -73,7 +73,7 @@ class LoginController extends CommonController
                 //$user = User::where('user_mobile',$input['user_mobile'])->get();
                 //$user = User::where('user_id',1)->get();
                 $user= User::first();
-                dd($user->user_id);
+                dd($user);
                 $_password=Crypt::decrypt($user->user_passwd);
                 if ($input['password_o']==$_password){
                     $user->user_passwd = Crypt::encrypt($input['password']);
