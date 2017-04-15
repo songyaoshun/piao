491090a4828deb8350cdb6a09c751d69db4b954d
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 12:01:37 +0800

    update

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="{{asset('resources/style/css/ch-ui.admin.css')}}">
-	<link rel="stylesheet" href="{{asset('resources/style/font/css/font-awesome.min.css')}}">
-    <script type="text/javascript" src="{{asset('resources/style/js/jquery.js')}}"></script>
-    <script type="text/javascript" src="{{asset('resources/style/js/ch-ui.admin.js')}}"></script>
+	<link rel="stylesheet" href="{{asset('resources/views/style/css/ch-ui.admin.css')}}">
+	<link rel="stylesheet" href="{{asset('resources/views/style/font/css/font-awesome.min.css')}}">
+    <script type="text/javascript" src="{{asset('resources/views/style/js/jquery.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/views/style/js/ch-ui.admin.js')}}"></script>
 </head>
 
     @yield('content')
