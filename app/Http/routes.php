f1f649b384bf1668ceee14cf87811d2530322fe6
zhuwen <zhuwen975@pingan.com.cn>
2017-04-15 11:33:22 +0800

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,74 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-//http://blog.hd/user/dfsdfds
-Route::get('user/{id}', function ($id) {
-    return 'User '.$id;
-})->where('id', '[0-9]+');
-
-
-//Route::get('posts/{post}/comments/{comment?}', function ($postId=0, $commentId=null) {
-//    return $postId .'----'.$commentId;
-//});
-
-//Route::get('user_{id}', function ($i) {
-//    return 'User '.$i;
-//});
-//
-//Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
-//    return $commentId;
-//});
-
-//
-//Route::get('/hd', function () {
-//    echo 'get';
-//});
-//
-//Route::post('/hd', function () {
-//    echo 'post';
-//});
-//
-//Route::put('/hd', function () {
-//    echo 'put';
-//});
-//
-//Route::delete('/hd', function () {
-//    echo 'delete';
-//});
-//
-//Route::patch('/hd', function () {
-//    echo 'patch';
-//});
-//
-//Route::options('/hd', function () {
-//    echo 'options';
-//});
-//
-//
-//Route::match(['get', 'post','patch'], '/test', function () {
-//    echo 'match';
-//});
-//
-//Route::any('foo', function () {
-//    echo 'foo';
-//});
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+Route::get('/', 'IndexController@index');
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('user/login','User/LoginController@login');
+    Route::get('user/register','User/LoginController@register');
+});
+
+Route::group(['middleware' => ['user.login']], function () {
+    Route::get('user/authen','User/UserController@authen');
+    Route::get('user/subpiao','User/UserController@subpiao');
 });
